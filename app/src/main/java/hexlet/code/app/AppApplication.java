94d285cc71518b3765fc06059eda39515481d5e5
ivan <ivanprikhodko2004@gmail.com>
2023-03-29 16:51:35 +0300

    two-step

--- app/src/main/java/hexlet/code/app/AppApplication.java
+++ app/src/main/java/hexlet/code/app/AppApplication.java
@@ -1,16 +1,12 @@
 package hexlet.code.app;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class AppApplication {
+
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load(); // Load environment variables from .env file
-        String databaseUrl = dotenv.get("ACTIVE_PROFILE");
         SpringApplication.run(AppApplication.class, args);
     }
 
--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,13 +1,16 @@
-package hexlet.code;
+package hexlet.code.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AppApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
 
 }
